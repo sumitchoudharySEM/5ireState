5569e0b702e0391149bfb181b018e7a74b35ae97
urmaliyadivyanshu31 <urmaliyadiv04@gmail.com>
2023-04-13 01:08:10 +0530

    Added Configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useContract, useContractWrite } from "@thirdweb-dev/react";
 
 
 
 function App() {
+  const { contract } = useContract("0x457c96f5407007F871FD6A94A3045D8E56d6121B");
+  const { mutateAsync: approve, isLoading } = useContractWrite(contract, "approve");
 
-
+  const call = async (to, tokenId) => {
+    try {
+      const data = await approve({ args: [to, tokenId] });
+      console.info("contract call success", data);
+    } catch (err) {
+      console.error("contract call failure", err);
+    }
+  }
   return (
-    <div className="App">
-    </div>
+    <div>
+    <button onClick={() => call("0x1234567890", 1)} disabled={isLoading}>
+      {isLoading ? "Loading..." : "Approve Token"}
+    </button>
+  </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { _5irechain } from "@thirdweb-dev/chains";
+import { ThirdwebProvider} from "@thirdweb-dev/react";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThirdwebProvider activeChain={ _5irechain }>
     <App />
-  </React.StrictMode>,
+  </ThirdwebProvider>,
+  </React.StrictMode>
 )
